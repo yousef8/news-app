8d1fd07835c740a16f8145ea6ebb0cc8cfc2ca98
Yousef Elsayed <yousef4511@outlook.com>
2024-06-14 11:34:20 +0300

    Create login & singup forms

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,64 @@
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type LoginInput = {
+  email: string;
+  password: string;
+};
 
 const Login: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>();
+
+  const onSubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
   return (
-    <div>
-      <h2>Login Page</h2>
-      <p>Please log in.</p>
-    </div>
+    <>
+      <div className="mt-5">
+        <h2 className="mb-4">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-floating mb-3">
+            <input
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+                  message: "Invalid email format",
+                },
+              })}
+              type="email"
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
+              placeholder="name@example.com"
+            />
+            <label htmlFor="email">Email</label>
+            <div className="invalid-feedback">{errors.email?.message}</div>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Min length is 8",
+                },
+              })}
+              id="password"
+              type="password"
+              className={`form-control ${errors.password ? "is-invalid" : ""}`}
+              placeholder="name@example.com"
+            />
+            <label htmlFor="password">Password</label>
+            <div className="invalid-feedback">{errors.password?.message}</div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,10 +1,76 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type SignUpInput = {
+  fullName: string;
+  email: string;
+  password: string;
+};
 
 const SignUp: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpInput>();
+
+  const onSubmit = (data: SignUpInput) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h2>Sign Up Page</h2>
-      <p>Please sign up.</p>
+    <div className="mt-5">
+      <h2 className="mb-4">Sign Up</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-floating mb-3">
+          <input
+            id="fullName"
+            {...register("fullName", { required: "Full name is required" })}
+            type="text"
+            className={`form-control ${errors.fullName ? "is-invalid" : ""}`}
+            placeholder="Full Name"
+          />
+          <label htmlFor="fullName">Full Name</label>
+          <div className="invalid-feedback">{errors.fullName?.message}</div>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            id="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+                message: "Invalid email address",
+              },
+            })}
+            type="email"
+            className={`form-control ${errors.email ? "is-invalid" : ""}`}
+            placeholder="Email"
+          />
+          <label htmlFor="email">Email</label>
+          <div className="invalid-feedback">{errors.email?.message}</div>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            id="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+            type="password"
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
+            placeholder="Password"
+          />
+          <label htmlFor="password">Password</label>
+          <div className="invalid-feedback">{errors.password?.message}</div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 };
