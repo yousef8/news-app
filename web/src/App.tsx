1792d57a6ae0ac67d6f161ad38bbfdc32971bd1e
Yousef Elsayed <yousef4511@outlook.com>
2024-06-14 09:23:46 +0300

    Create header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
+      <Header />
       <div className="container-lg">
         <h1>Hello World</h1>
       </div>
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/newspaper.png"
+            alt="Logo"
+            width="30"
+            height="30"
+            className="d-inline-block align-text-top me-2"
+          />
+          News App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sources
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
