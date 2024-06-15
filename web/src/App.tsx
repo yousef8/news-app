b9e28ad5d033fd442f438603ba2ab02c8dbd5da3
Yousef Elsayed <yousef4511@outlook.com>
2024-06-15 11:27:25 +0300

    Increase font size and misc styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 import Header from "./components/Header/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
