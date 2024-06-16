90e0d0852e9dac90a10607eaeb050697472bd58c
Yousef Elsayed <yousef4511@outlook.com>
2024-06-16 18:06:36 +0300

    Style: Change loading icon size

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -4,7 +4,7 @@ import { PacmanLoader } from "react-spinners";
 const Loading: React.FC = () => {
   return (
     <div className="loading-container d-flex justify-content-center align-items-center vh-100">
-      <PacmanLoader size={150} color={"#123abc"} loading={true} />
+      <PacmanLoader size={80} color={"#123abc"} loading={true} />
     </div>
   );
 };
